8871515523b6d26386e9c7d1c07ce1359afe779d
Narenthiran Y <narenthiran.y@teleapps.com>
2021-10-13 17:26:37 +0530

    build updates

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom'; 
 
 ReactDOM.render(
-  <HashRouter basename="./">
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
