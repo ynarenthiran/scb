ebba69372e9a6c55c3a3fc3419d5f62054f51d7d
Narenthiran Y <narenthiran.y@teleapps.com>
2021-11-17 10:56:47 +0530

    build changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import { Component } from 'react';
 import Tab from './components/tab/tab';
 import Captcha from './components/captcha/Captcha';
 
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Redirect, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Route path="/captcha" component={Captcha} />
         <Route path="/booking" component={Tab} />
         <Redirect to="/booking" />
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
